1c11d7bfccbfc3c33c7ea7b16757273211773038
joowce <joy@github.com>
2019-06-04 15:16:06 +0900

    add: - transaction manager: add transaction 추가 --- remove: --- change: ---

--- src/manager/transactionManager.py
+++ src/manager/transactionManager.py
@@ -1,4 +1,13 @@
 import time
+import service.Validator as Validator
+import logging
+from enum import Enum
+
+
+class TransactionResult(Enum):
+    Fail = -1
+    Success = 1
+    Full = 2
 
 
 class TransactionManager:
@@ -13,3 +22,13 @@ class TransactionManager:
             'data': data,
             'timestamp': time.time(),
         }
+
+    def add_transaction(self, transaction):
+        # TODO: pass valid_transaction parameter
+        if Validator.valid_transaction():
+            logging.error('not valid transaction')
+            return TransactionResult.Fail
+        self.pool.append(transaction)
+        return TransactionResult.Full \
+            if len(self.pool) == 10 \
+            else TransactionResult.Success
