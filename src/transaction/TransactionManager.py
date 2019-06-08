071c8313c99e87c17b6c19794107b651b695d70c
Joowce <minjw1026@naver.com>
2019-06-09 01:23:39 +0900

    add: - Block Manager --- remove: --- change: - transaction manager: get_transaction

--- src/transaction/TransactionManager.py
+++ src/transaction/TransactionManager.py
@@ -1,4 +1,5 @@
-import service.Validator as Validator
+from service import Validator
+from block import BlockManager
 import logging
 from enum import Enum
 
@@ -6,7 +7,6 @@ from enum import Enum
 class TransactionResult(Enum):
     Fail = -1
     Success = 1
-    Full = 2
 
 
 class TransactionManager:
@@ -17,7 +17,11 @@ class TransactionManager:
         if Validator.valid_transaction(transaction):
             logging.error('not valid transaction')
             return TransactionResult.Fail
+
         self.pool.append(transaction)
-        return TransactionResult.Full \
-            if len(self.pool) == 10 \
-            else TransactionResult.Success
+
+        if len(self.pool) % 10 == 0:
+            logging.info("pool has new 10 transactions -> notice to block manager")
+            BlockManager.notice_making_block(self.pool[-10:])
+
+        return TransactionResult.Success
