7879d8ca7e0c7906c2911ad43b0d15d27fe70c4c
Joowce <minjw1026@naver.com>
2019-06-12 20:49:34 +0900

    add: - crypto: sign --- remove: --- change:

--- src/util/Crypto.py
+++ src/util/Crypto.py
@@ -7,6 +7,11 @@ def generate_key():
     return str(private_key.to_string()), str(public_key.to_string())
 
 
+def sign(private_key, data):
+    sk = SigningKey.from_string(private_key)
+    return sk.sign(data)
+
+
 def verify_signature(public_key, signature, data):
     vk = VerifyingKey.from_string(public_key, NIST384p)
     return vk.verify(signature, data)
