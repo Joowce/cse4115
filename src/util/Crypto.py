ebc9090ba0ac5a5a17e67dec41b17b9abd41a74c
Joowce <minjw1026@naver.com>
2019-06-12 21:06:36 +0900

    add: - crypto: testcode --- remove: --- change: - crypto: key encode, data encode

--- src/util/Crypto.py
+++ src/util/Crypto.py
@@ -4,14 +4,22 @@ from ecdsa import SigningKey, VerifyingKey, NIST384p
 def generate_key():
     private_key = SigningKey.generate(curve=NIST384p)
     public_key = private_key.get_verifying_key()
-    return str(private_key.to_string()), str(public_key.to_string())
+    return private_key.to_string().hex(), public_key.to_string().hex()
 
 
 def sign(private_key, data):
-    sk = SigningKey.from_string(private_key)
-    return sk.sign(data)
+    sk = SigningKey.from_string(bytes.fromhex(private_key), curve=NIST384p)
+    return sk.sign(data.encode())
 
 
 def verify_signature(public_key, signature, data):
-    vk = VerifyingKey.from_string(public_key, NIST384p)
-    return vk.verify(signature, data)
+    vk = VerifyingKey.from_string(bytes.fromhex(public_key), curve=NIST384p)
+    return vk.verify(signature, data.encode())
+
+
+if __name__ == '__main__':
+    Sk, Vk = generate_key()
+    print(bytes.fromhex(Sk))
+    msg = input()
+    sig = sign(Sk, msg)
+    print(verify_signature(Vk, sig, msg))
